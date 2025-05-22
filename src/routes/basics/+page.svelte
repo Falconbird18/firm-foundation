02a45f700592a761d8bdb3d49d869d6202756e37
falconbird18 <austintallent25@outlook.com>
2025-05-22 11:13:57 -0700

    Remove duplicate content.

--- src/routes/basics/+page.svelte
+++ src/routes/basics/+page.svelte
@@ -191,37 +191,6 @@
             <div class="sub-cards-container">
                 <div class="sub-card">
                     <img src="creation.png" class="card-image" />
-                    <h2>The first sin</h2>
-                    <p>
-                        Adam and Eve sinned when they ate the fruit of the tree
-                        of the knowledge of good and evil
-                        <BibleCitation verseRef="Genesis 3:6" verseText={'When the woman saw that the tree was good for food, and that it was a delight to the eyes, and that the tree was desirable to make _one_ wise, she took from its fruit and ate; and she gave also to her husband with her, and he ate.'} />,
-                        which God had
-                        commanded them not to eat
-                        <BibleCitation verseRef="Genesis 2:16-17" verseText={'[v16]The Lord God commanded the man, saying, "From any tree of the garden you may eat freely; [v17] but from the tree of the knowledge of good and evil you shall not eat, for in the day that you eat from it you will surely die."'} />
-                        after they had been deceived
-                        by Satan, a fallen archangel who was in the form of a
-                        snake
-                        <BibleCitation verseRef="Revelation 12:9" verseText={'And the great dragon was thrown down, the serpent of old who is called the devil and Satan, who deceives the whole world; he was thrown down to the earth, and his angels were thrown down with him.'} />.
-                        By sinning, Adam and Eve disobeyed God and
-                        brought death and disease into the world
-                        <BibleCitation verseRef="Genesis 3:17-19" verseText={'[v17]Then to Adam He said, "Because you have listened to the voice of your wife, and have eaten from the tree about which I commanded you, saying, \'You shall not eat from it\'; Cursed is the ground because of you; [br] In toil you will eat of it [br] All the days of your life. [v18]\"Both thorns and thistles it shall grow for you; [br] And you will eat the plants of the field; By the sweat of your face [br] You will eat bread, [br] Till you return to the ground [br] Because from it you were taken; [br] For you are dust, [br] And to dust you shall return.\""'} wide/>
-                        <BibleCitation verseRef="Romans 5:12" verseText={'Therefore, just as through one man sin entered into the world, and death through sin, and so death spread to all men, because all sinned'} />
-                        and the whole
-                        of creation was affected by their sin
-                        <BibleCitation verseRef="Romans 8:20-22" verseText={'[v20]For the creation was subjected to futility, not willingly, but because of Him who subjected it, in hope [v21] that the creation itself also will be set free from its slavery to corruption into the freedom of the glory of the children of God. [v22]For we know that the whole creation groans and suffers the pains of childbirth together until now.'} />.
-                    </p>
-                </div>
-                <div class="sub-card">
-                    <img src="creation.png" class="card-image" />
-                    <h2>The expulsion</h2>
-                    <p>
-                    </p>
-                </div>
-            </div>
-            <div class="sub-cards-container">
-                <div class="sub-card">
-                    <img src="creation.png" class="card-image" />
                     <h2>The promise</h2>
                     <p>
                     </p>
